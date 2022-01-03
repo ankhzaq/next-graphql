85a0e163a3972c23c630fd6f41c041033977bf54
zaquiel.rodriguez <zaquielverse@gmail.com>
2022-01-03 07:31:57 +0100

    first commit -  project base

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+const main = async () => {
+    console.log("__dirname, ", __dirname);
+};
+main().catch((err) => {
+    console.log("err: ", err);
+});
+console.log("hello world!");
+//# sourceMappingURL=index.js.map
---
+++ src/entities/Post.ts
@@ -0,0 +1,15 @@
+import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
+
+@Entity()
+export class Post {
+  @PrimaryKey()
+  id!: number;
+
+  @Property()
+  createdAt = new Date();
+
+  @Property({ onUpdate: () => new Date() })
+  updateAt = new Date();
+
+  title!: string;
+}
