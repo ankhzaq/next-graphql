82a20c9a8ed6e310fb455ab63681c112253f6214
zaquiel.rodriguez <zaquielverse@gmail.com>
2022-04-12 09:14:33 +0200

    feature - adding dialog logout disabled in environment

--- web/src/components/NavbBar.tsx
+++ web/src/components/NavbBar.tsx
@@ -1,13 +1,27 @@
-import { Box, Button, Flex, Heading, Link } from '@chakra-ui/react';
+import {
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  AlertTitle,
+  Box,
+  Button,
+  CloseButton,
+  Flex,
+  Heading,
+  Link, useToast
+} from '@chakra-ui/react';
 import NextLink from 'next/link';
 import { useLogoutMutation, useMeQuery } from '../generated/graphql';
-import { isServer } from '../utils/isServer';
 import { useRouter } from 'next/router';
-interface NavBarProps {
+import { useState } from 'react';
 
-}
+const DURATION_LOGOUT_DISABLED = 2500;
+
+interface NavBarProps {}
 
 export const NavBar: React.FC<NavBarProps> = ({}) => {
+  const [showLogoutDisabledDialog, setShowLogoutDisabledDialog] = useState(false);
+  const toast = useToast()
   const router = useRouter();
   const [{ fetching: logoutFetching }, logout] = useLogoutMutation();
   const [{ data, fetching }] = useMeQuery();
@@ -32,6 +46,12 @@ export const NavBar: React.FC<NavBarProps> = ({}) => {
   } else {
     body = (
       <Flex zIndex={1} position="sticky" bg="tan" p={4} top="0" align="center">
+        {showLogoutDisabledDialog && (
+          <Alert status='error'>
+            <AlertIcon />
+            <AlertDescription>Logout is not available in the environment [heroku and cookies are not friends :) ]</AlertDescription>
+          </Alert>
+        )}
           <NextLink href="/create-post">
             <Button as={Link} mr={4}>
               create post
@@ -39,8 +59,23 @@ export const NavBar: React.FC<NavBarProps> = ({}) => {
           </NextLink>
         <Box mr={2}>{data.me.username}</Box>
         <Button variant="link" onClick={async () => {
-          await logout();
-          router.reload()
+          if (process.env.HEROKU_FIX_USER) {
+            setShowLogoutDisabledDialog(true);
+            setTimeout(() => {
+              setShowLogoutDisabledDialog(false)
+            }, DURATION_LOGOUT_DISABLED);
+
+            toast({
+              title: 'Logout not available in the environment',
+              description: "heroku and cookies are not friends :)",
+              status: 'info',
+              duration: DURATION_LOGOUT_DISABLED,
+              isClosable: true,
+            });
+          } else {
+            await logout();
+            router.reload()
+          }
         }} isLoading={logoutFetching}>logout</Button>
     </Flex>)
   }
